b02c01b380ef0f71012f74896944f7f92f8a96c0
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2024-05-26 20:13:25 -0300

    add func getProdutById, add initial page prod details

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { RegisterAtendComponent } from './pages/register-atend/register-atend.component';
+import { ListAtendComponent } from './pages/list-atend/list-atend.component';
+import { EditAtendComponent } from './pages/edit-atend/edit-atend.component';
+import { RegisterProductComponent } from './pages/register-product/register-product.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import {HomeComponent} from './pages/home/home.component'
+import {ProductDetailComponent} from './pages/product-detail/product-detail.component'
+
+export const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"product/:id", component: ProductDetailComponent},
+  {path:"resgister-atend", component: RegisterAtendComponent},
+  {path:"list-atend", component: ListAtendComponent},
+  {path:"edit-atend/:id", component: EditAtendComponent},
+  {path:"register-product", component: RegisterProductComponent },
+  {path:"register", component: RegisterComponent},
+  {path:"login", component: LoginComponent},
+];
